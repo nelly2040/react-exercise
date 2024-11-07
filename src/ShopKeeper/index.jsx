e41002e0828b5c58a191340eabf5a5a1692f184f
Beth Kimani <kbwaceke@gmail.com>
2024-11-07 11:09:04 +0300

    completed the exercise

--- src/ShopKeeper/index.jsx
+++ src/ShopKeeper/index.jsx
@@ -1,26 +1,37 @@
-import items from "./items"
+import React from "react";
+import items from "./items";
+import "./index.css";
 
-//Create a Table row component
-
-import "./index.css"
-
-function ShopKeeperApp(){
-    return <div>
-        <table>
-            <thead>
-                <th>no</th>
-                <th>Name</th>
-                <th>unit_price</th>
-                <th>qty</th>
-            </thead>
-            <tbody>
-                {items.map((item)=>{
-                    return <td></td>
-                })}
-            </tbody>
-        </table>
+function ShopKeeperApp() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Unit Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Render rows for actual items */}
+          {items.map((item, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              {/* Format unit price with commas */}
+              <td>{item.unit_cost.toLocaleString()}</td>
+              <td>{item.qty}</td>
+              {/* Calculate total and format with commas */}
+              <td>{(item.unit_cost * item.qty).toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
+  );
 }
 
-
-export default ShopKeeperApp
+export default ShopKeeperApp;
